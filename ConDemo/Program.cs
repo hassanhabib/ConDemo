8e7a93b9ff12ce085b28de8f4d72ef17141e86f2
Hassan Habib <hassanhabib@live.com>
2022-12-11 13:36:27 -0800

    Add project files.

---
+++ ConDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿#define UniverseFeature
+
+using System;
+using System.Diagnostics;
+
+namespace ConDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Foo();
+            Console.WriteLine("Hello, World!");
+        }
+
+        [Conditional("UniverseFeature")]
+        static void Foo() =>
+            Console.WriteLine("Hello, Universe!");
+    }
+}
